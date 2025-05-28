9f181fbf5de85c0241d241ad53e666f810b560ca
sampurna343 <31670067+sampurna343@users.noreply.github.com>
2025-05-28 23:40:20 +0530

    Update Two-Sum.java

--- Two-Sum.java
+++ Two-Sum.java
@@ -1,5 +1,19 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-    
+        Map<Integer,Integer> map = new HashMap<>();
+        int comp;
+        int[] result = new int[2];
+        for(int i=0;i<nums.length;i++){
+            comp=target-nums[i];
+            if(map.containsKey(comp)){
+                result[0]=i;
+                result[1]=map.get(comp);
+                return result;
+            }
+            else{
+                map.put(nums[i],i);
+            }
+        }
+    return result;    
     }
 }
