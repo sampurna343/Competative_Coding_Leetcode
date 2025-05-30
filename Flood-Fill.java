9e800e3053473b4f858a04533de0912502dd8848
sampurna343 <31670067+sampurna343@users.noreply.github.com>
2025-05-30 22:32:21 +0530

    Update Flood-Fill.java

--- Flood-Fill.java
+++ Flood-Fill.java
@@ -1,5 +1,34 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        
+        int currentColor=image[sr][sc];
+        int[][] dic={{1,0},{0,1},{-1,0},{0,-1}};
+
+        if(currentColor!=color){
+        dfs(image, sr, sc, color,dic,currentColor);
+        }
+
+        return image;
+    }
+
+    private boolean isValid(int sr,int sc,int[][] image){
+        if(sr==-1 || sc==-1 || sr==image.length || sc==image[0].length){
+            return false;
+        }
+        return true;
+    }
+
+    private void dfs(int[][] image, int sr, int sc, int color,int[][] dic,int currentColor){
+        if(!isValid(sr,sc,image) || image[sr][sc]!=currentColor){
+            return;
+        }
+
+        image[sr][sc]=color;
+
+        for(int[] d:dic){
+            int n_sr=sr+d[0];
+            int n_sc=sc+d[1];
+            dfs(image, n_sr, n_sc, color,dic,currentColor);
+        }
+        return;
     }
 }
