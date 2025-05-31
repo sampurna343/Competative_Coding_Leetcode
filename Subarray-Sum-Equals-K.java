8b0754524295776b0fdb46147f392c87d10bb359
sampurna343 <31670067+sampurna343@users.noreply.github.com>
2025-05-31 16:29:31 +0530

    Update Subarray-Sum-Equals-K.java

--- Subarray-Sum-Equals-K.java
+++ Subarray-Sum-Equals-K.java
@@ -1,27 +1,26 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        
-        Map<Integer, Integer> sumMap = new HashMap<>();
-        int currSum = 0, count = 0;
+        int count =0;
+        int sum=0;
+        HashMap<Integer,Integer> map= new HashMap<>();
+
         for(int i=0;i<nums.length;i++){
-            currSum += nums[i];
-            
-            //if prefixSum is equal to the target
-            //then it is a subarray that is equal to k
-            if(currSum==k){
+
+            //full till i subarray check the target sum
+            sum=sum+nums[i]; 
+            if(sum==k){
                 count++;
             }
             
-            //check in the map that (currSum-k) is present or not
-            //if present, we can say there is a subarray that is equal to k
-            if(sumMap.containsKey(currSum-k)){
-                count = count + sumMap.get(currSum-k);
+            //previous subarray i-1 check the target sum
+            int temp=sum-k;
+            if(map.containsKey(temp)){
+                count=count+map.get(temp);
             }
-            
-            //put the prefix sum in the map
-            sumMap.put(currSum, sumMap.getOrDefault(currSum,0)+1);
+
+            map.put(sum,map.getOrDefault(sum,0)+1);
         }
-        
+
         return count;
     }
 }
